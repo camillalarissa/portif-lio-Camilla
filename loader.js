9f7425b32f2357037cbc5d9181570b788f060a4f
camillalarissa <115382914+camillalarissa@users.noreply.github.com>
2024-02-07 22:03:23 -0300

    commit

---
+++ loader.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var loadingContainer = document.getElementById("loading-container");
+  var contentContainer = document.getElementById("content-container");
+
+  if (loadingContainer && contentContainer) {
+    simulateAsyncLoading().then(function () {
+      loadingContainer.style.display = "none";
+      contentContainer.style.display = "block";
+    });
+  }
+});
+
+function simulateAsyncLoading() {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, 2000);
+  });
+}
---
+++ script.js
@@ -0,0 +1,43 @@
+/*MENU*/
+
+function openNav() {
+  document.getElementById("openNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("openNav").style.width = "0%";
+}
+
+/*PROJETO*/
+
+const initSlider = () => {
+  const cardList = document.querySelector(".slider .slides");
+  const slideButtons = document.querySelectorAll(".slider .slider-buttons");
+  slideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prev" ? -1 : 1;
+      const scrollAmount = cardList.clientWidth * direction;
+      cardList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+  });
+};
+
+window.addEventListener("load", initSlider);
+
+/*COPYRTING*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const yearSpan = document.getElementById("year");
+  const currentYear = new Date().getFullYear();
+  yearSpan.textContent = currentYear;
+});
+
+/*DARK MENU*/
+
+let darK = document.getElementById("darK");
+let body = document.querySelector("body");
+
+darK.addEventListener("click", () => {
+  darK.classList.toggle("dark1");
+  body.classList.toggle("dark1");
+});
