1dc6d240d3f05baf5ba22c87503046da7b1a08f9
camillalarissa <115382914+camillalarissa@users.noreply.github.com>
2024-02-12 23:53:30 -0300

    atualizando

--- script.js
+++ script.js
@@ -31,4 +31,19 @@ document.addEventListener("DOMContentLoaded", function () {
   const currentYear = new Date().getFullYear();
   yearSpan.textContent = currentYear;
 });
-      
+
+/*SCROLL*/
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const elements = document.querySelectorAll(".hidden");
+
+elements.forEach((element) => myObserver.observe(element));
